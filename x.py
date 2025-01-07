9c4b88ab0cc2c789048684a35eaae9266a00d287
Pramesh <prameshgurav79286@gmail.com>
2025-01-07 10:59:35 +0530

    1st python code

---
+++ x.py
@@ -0,0 +1,16 @@
+#  Print the phrase "Hello, World!" using the print statement. 
+
+print("Hello","World", sep=' ', end='\n', flush=False)
+
+# Print three different messages on separate lines.
+
+print("Hello")
+print("World")
+print("How are you?")
+
+# Print three different messages on separate lines using one print statement.
+
+print("Hello","World","How are you?" , sep="\n")
+
+#Concatenate and print two variables
+
