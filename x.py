ca1549ffaa6c4424715e63ee84d814900f1075d3
Pramesh <prameshgurav79286@gmail.com>
2025-01-07 18:22:03 +0530

    practice question

--- x.py
+++ x.py
@@ -1,3 +1,5 @@
+'''# ---------- Q.1 Print Statement ---------- 
+
 #  Print the phrase "Hello, World!" using the print statement. 
 
 print("Hello","World", sep=' ', end='\n', flush=False)
@@ -13,4 +15,69 @@ print("How are you?")
 print("Hello","World","How are you?" , sep="\n")
 
 #Concatenate and print two variables
+
+a = 10
+b = 20
+
+print(a+b)
+
+# Print three words separated by a specific character ‘$’.
+print("Hello", "World", "How are you?", sep='$')
+
+# Print two messages on the same line using the end parameter.
+
+print("Hi!,What are you doing?", end=' ')
+print("Nothing. I am just chilling.")
+
+# Print a message with quotes around it.
+
+print("Hello World")
+
+# Print the result of a Boolean expression.
+
+print(10>5)
+
+
+# -------Q.2 Type casting: --------
+
+# Q1: Convert an integer to a float. 
+
+a =10
+print(float(a))
+
+# Q2: Convert a float to an integer.
+
+b = 10.5
+print(int(b))
+
+# Q3: Convert a string to an integer.
+
+c = "10"
+
+print(int(b))
+
+# Q4: Convert a string to a float.
+
+d = "10.5"
+
+print(float(d))
+
+# Q.5 Convert an integer to a string.
+
+e = 20
+
+print(str(e))'''
+
+# Q.6 Convert a float to a string. 
+
+a = 100.5
+
+print(str(a))
+
+# Q.7  Convert a binary string to an integer. 
+
+b = "1010"
+
+print(int(b,2))
+
 
