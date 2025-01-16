88c64db7525360a625e95f47322c84041b769f74
Pramesh <prameshgurav79286@gmail.com>
2025-01-16 18:12:07 +0530

    lect 2

---
+++ python-main/2. variable declaration and intialization.py
@@ -0,0 +1,21 @@
+# Initializing variables
+x = 10  # x is an integer
+print(x,type(x))
+name = "John"  # name is a string
+print(name,type(name))
+pi = 3.14159  # pi is a floating-point number
+print(pi,type(pi))
+booll = True  # booll is a boolean
+
+print(x,name,pi,booll)
+
+# You can also initialize multiple variables in a single line
+a, b, c = 5, "Bob", 2.5
+print('a: ',a)
+print('b: ',b)
+print('c: ',c)
+
+# Intializing 100 to all variable x,y,z 
+x=y=z=100
+print(x)
+
