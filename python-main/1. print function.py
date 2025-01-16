eebf227f5693c9f138e56074a2b32300bc1bf86b
Pramesh <prameshgurav79286@gmail.com>
2025-01-16 18:10:47 +0530

    print

---
+++ python-main/1. print function.py
@@ -0,0 +1,27 @@
+# Printing a simple string
+print("Avengers Assemble!")
+
+print()
+
+# Printing multiple items with a custom separator
+name = "Tony Stark"
+age = 30
+power=1000
+print(name, age,power,sep='++++')
+
+print()
+
+# Printing without a newline character at the end
+print("This",end='')
+print("is",end='')
+print("on", end='')
+print("the", end='')
+print("same", end='')
+print("line.")
+
+
+print()
+
+print("captain",end='*')
+print('america')
+print('roger')
