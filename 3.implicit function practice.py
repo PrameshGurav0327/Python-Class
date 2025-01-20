165759a18c67599bcf12cf4244cb20627bb58aa6
Pramesh <prameshgurav79286@gmail.com>
2025-01-20 10:58:03 +0530

    -Implicit and explicit type conversion

---
+++ 3.implicit function practice.py
@@ -0,0 +1,22 @@
+print("-----------Implicit and explicit type conversion--------------------")
+
+print("----------Implicitely calculating the integers and float----------------")
+
+x=5     
+y=2.5
+
+result=x+y
+print(result)
+print(type(result))
+
+print()
+
+print("--------------Explicit function----------------")
+
+a = 10.86
+z = int(a) # In this example we have given a datatype manually that's why the float value get converted into ineger so the output will be 10 not the 10.86
+print(z)
+
+print(type(z))
+
+
